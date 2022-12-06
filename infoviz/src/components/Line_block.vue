f6c21361a6a2e75b4ba6378333055d767d1fb790
markus.stanzl <markus.stanzl@jetbrains.com>
2022-12-06 16:14:22 +0100

    init

--- infoviz/src/components/Line_block.vue
+++ infoviz/src/components/Line_block.vue
@@ -1,15 +1,45 @@
 <template>
   <div>
-    line graph
+    <h3>Trend For: Germany</h3>
+    <div id="app">
+      <LineChart v-bind:class="[toggleClass]"/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Line_block"
-}
+  import LineChart from "./linechart.vue";
+
+  export default {
+    name: "Line_block",
+    components: {
+      LineChart
+  },
+
+  data() {
+    return {
+      toggleClass: "ani1"
+    };
+  },
+
+  methods: {
+    play() {
+      // toggle classes to animate the line draw
+      this.toggleClass == "ani1"
+      ? (this.toggleClass = "ani2")
+      : (this.toggleClass = "ani1");
+      }
+    }
+  };
 </script>
 
 <style scoped>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+  padding: 0px;
+}
 
 </style>

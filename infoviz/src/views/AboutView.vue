16ab6fbf990ff52fe174f8217fca9a6f1b493b0e
quirinmuller <Qui.Mueller@campus.lmu.de>
2022-12-07 12:06:00 +0100

    about page

--- infoviz/src/views/AboutView.vue
+++ infoviz/src/views/AboutView.vue
@@ -1,61 +1,66 @@
 <template>
-  <h1>
-    team
-  </h1>
-  <div class="role">
-    Frontend <br />
-    <br />
-    <br />
-    <br />
-    Backend
+  <div class="horizontal_flex">
+    <h1>
+      team
+    </h1>
+    <div class="role">
+      Frontend <br />
+      <br />
+      <br />
+      <br />
+      Backend
+    </div>
+    <div class="names">
+      Markus Stanzl <br />
+      Isabel Wanderwitz  <br />
+      Quirin Müller <br />
+      <br />
+      Leonard Ganz <br />
+      Robin Löbbert <br />
+    </div>
+    <div class="subjects">
+      Medieninformatik<br />
+      Medieninformatik<br />
+      Medieninformatik<br />
+      <br />
+      Informatik <br />
+      Computerlinguistik<br />
+    </div>
+
   </div>
-  <div class="names">
-    Markus Stanzl <br />
-    Isabel Wanderwitz  <br />
-    Quirin Müller <br />
-    <br />
-    Leonard Ganz <br />
-    Robin Löbbert <br />
-  </div>
-  <div class="subjects">
-   Medieninformatik<br />
-   Medieninformatik<br />
-   Medieninformatik<br />
-    <br />
-   Informatik <br />
-   Computerlinguistik<br />
-  </div>
+
 </template>
 
 <style>
   h1 {
     font-size: 28px;
-    position:absolute;
-    left:-400px;
-    top:-310px;
+    position:fixed;
+    left:100px;
+    top: 10px;
 
   }
 
   .role{
-    position:absolute;
-    left:-500px;
-    top:-250px;
+    position:fixed;
+    left:10px;
+    top:70px;
     width: 200px;
   }
 
   .names {
-    position:absolute;
-    left:-400px;
-    top:-250px;
+    position:fixed;
+    left:100px;
+    top:70px;
     width: 200px;
 
   }
 
   .subjects {
-    position:absolute;
-    left:-200px;
-    top:-250px;
-
-
+    position:fixed;
+    left:260px;
+    top:70px;
+  }
+  .horizontal_flex {
+    display: flex;
   }
 </style>

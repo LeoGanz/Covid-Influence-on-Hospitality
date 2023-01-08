f28454000684d52427ceb952166987786748586e
Robin Löbbert <robin.loebbert@hotmail.com>
2023-01-08 14:48:25 +0100

    added linechart for covid data

--- infoviz/src/components/linechart.vue
+++ infoviz/src/components/linechart.vue
@@ -1,276 +1,311 @@
-<!--
-
-Good example here bar chart ---[ https://stackoverflow.com/questions/48726636/draw-d3-axis-without-direct-dom-manipulation
-
--->
-
-<template xmlns:v-bind=""><div>
-  <svg width="200%" height="70%" viewBox="0 0 800 330"
-       preserveAspectRatio="xMidYMid meet" >
-
-   
-
+<template>
+  <div v-if="!covidCasesStore.initialized">Loading...</div>
+  <svg
+    id="svg"
+    :width="chart.width"
+    :height="chart.height"
+    :viewBox="[0, 0, chart.width, chart.height]"
+    @pointerenter="pointerentered"
+    @pointermove="pointermoved"
+    @pointerleave="pointerleft"
+    @touchstart="(event) => event.preventDefault()"
+    v-else
+  >
+    <g
+      id="xaxis"
+      :transform="`translate(0,${chart.height - chart.marginBottom})`"
+    ></g>
+    <g id="yaxis" :transform="`translate(${chart.marginLeft},0)`"></g>
+    <g
+      id="paths"
+      fill="none"
+      :stroke="typeof chart.color === 'string' ? chart.color : null"
+      :stroke-linecap="chart.strokeLinecap"
+      :stroke-linejoin="chart.strokeLinejoin"
+      :stroke-width="chart.strokeWidth"
+      :stroke-opacity="chart.strokeOpacity"
+    ></g>
+    <g id="dot" display="none">
+      <circle r="2.5"></circle>
+      <text
+        font-size="15"
+        text-anchor="middle"
+        text-color="black"
+        y="-8"
+      ></text>
+    </g>
   </svg>
-
-  <p class='text' > {{chartDefaults.title}}</p>
-</div>
 </template>
 
 <script>
-//
-// <g class='lineChart' v-bind:transform="translate">
-//<axis class='yA' v-bind:scales="getScales().yAxis" v-bind:chartDefaults='chartDefaults' v-bind:data='data' v-bind:trns='trnsY'/>
-//    <axis class='xA' v-bind:scales="getScales().xAxis" v-bind:chartDefaults='chartDefaults' v-bind:data='data' v-bind:trns='trnsX()'/>
-//    <axis class='grid' v-bind:scales="getScales().yGrid" v-bind:chartDefaults='chartDefaults' v-bind:data='data' v-bind:trns='trnsY' v-bind:style="{opacity: chartDefaults.gridOpacity}"/>
-//    <path class='line' :d="line" />
-//  </g>
-//
 import * as d3 from "d3";
-import Axis from "./axis.vue";
+import { useCovidCasesStore } from "@/stores/covidCases.js";
 export default {
   name: "vue-line-chart",
-  components: {
-    axis: Axis // Using reusable component to draw x,y axis and Grid.
-  },
+  components: {},
   data() {
-    return {
-      data: [
-        {
-          day: "01-11-2016",
-          count: 80
-        },
-        {
-          day: "02-12-2016",
-          count: 250
-        },
-        {
-          day: "03-13-2016",
-          count: 150
-        },
-        {
-          day: "04-14-2016",
-          count: 496
-        },
-        {
-          day: "05-15-2016",
-          count: 140
-        },
-        {
-          day: "06-16-2016",
-          count: 380
-        },
-        {
-          day: "07-17-2016",
-          count: 140
-        },
-        {
-          day: "08-17-2016",
-          count: 240
-        },
-        {
-          day: "09-18-2016",
-          count: 100
-        },
-        {
-          day: "10-18-2016",
-          count: 260
-        },
-        {
-          day: "11-18-2016",
-          count: 100
-        },
-        {
-          day: "12-18-2016",
-          count: 150
-        }
-      ],
-      chartDefaults: {
-        width: 800,
-        height: 300,
-        chartId: "linechart-vue",
+    const covidCasesStore = useCovidCasesStore();
 
-        margin: {
-          top: 5,
-          right: 5,
-          bottom: 15,
-          left: 50
-        },
-        gridOpacity: 1,
-        data: []
+    return {
+      covidCasesStore: covidCasesStore,
+      chart: {
+        x: (d) => d.day,
+        y: (d) => d.value,
+        z: (d) => d.category,
+        //x: d => d., // given d in data, returns the (temporal) x-value
+        //y: ([, y]) => y, // given d in data, returns the (quantitative) y-value
+        //z: () => 1, // given d in data, returns the (categorical) z-value
+        title: undefined, // given d in data, returns the title text
+        defined: undefined, // for gaps in data
+        curve: d3.curveLinear, // method of interpolation between points
+        marginTop: 20, // top margin, in pixels
+        marginRight: 30, // right margin, in pixels
+        marginBottom: 30, // bottom margin, in pixels
+        marginLeft: 60, // left margin, in pixels
+        width: 600, // outer width, in pixels
+        height: 300, // outer height, in pixels
+        xType: d3.scaleUtc, // type of x-scale
+        xDomain: undefined, // [xmin, xmax]
+        xRange: undefined, // [left, right]
+        yType: d3.scaleLinear, // type of y-scale
+        yDomain: undefined, // [ymin, ymax]
+        yRange: undefined, // [bottom, top]
+        yFormat: undefined, // a format specifier string for the y-axis
+        yLabel: "Cases", // a label for the y-axis
+        zDomain: undefined, // array of z-values
+        color: "currentColor", // stroke color of line, as a constant or a function of *z*
+        strokeLinecap: undefined, // stroke line cap of line
+        strokeLinejoin: undefined, // stroke line join of line
+        strokeWidth: 1.5, // stroke width of line
+        strokeOpacity: undefined, // stroke opacity of line
+        mixBlendMode: "multiply", // blend mode of lines
       },
-      line: "",
-      //Translate co-ords for chart, x axis and yaxis. This is injected into template
-      translate: "translate(" + 50 + "," + 5 + ")",
-      trnsY: "translate(0,0)",
-      trnsX: this.getTrnsx,
-      toggleClass: true
     };
   },
-  mounted() {
-    var scale = {};
-    //Kick off drawing chart once component is mounted
-    this.calculatePath();
+  async mounted() {
+    await this.covidCasesStore.initValues();
+    this.renderChart();
+  },
+  computed: {
+    data() {
+      return this.covidCasesStore.cases.map((value) => {
+        value.day = new Date(value.day).getTime();
+        value.category = "Covid";
+        return value;
+      });
+    },
+    // // Compute values.
+    //  const X = d3.map(data, x);
+    xRange() {
+      if (this.chart.xRange === undefined) {
+        return [
+          this.chart.marginLeft,
+          this.chart.width - this.chart.marginRight,
+        ];
+      }
+      return this.chart.xRange;
+    },
+    yRange() {
+      if (this.chart.yRange === undefined) {
+        return [
+          this.chart.height - this.chart.marginBottom,
+          this.chart.marginTop,
+        ];
+      }
+      return this.chart.yRange;
+    },
+    X() {
+      return d3.map(this.data, this.chart.x);
+    },
+    //  const Y = d3.map(data, y);
+    Y() {
+      return d3.map(this.data, this.chart.y);
+    },
+    //  const Z = d3.map(data, z);
+    Z() {
+      return d3.map(this.data, this.chart.z);
+    },
+    //  const O = d3.map(data, (d) => d);
+    O() {
+      return d3.map(this.data, (d) => d);
+    },
+    defined() {
+      if (this.chart.defined === undefined) {
+        return (d, i) => !isNaN(this.X[i]) && !isNaN(this.Y[i]);
+      }
+      return this.chart.defined;
+    },
+    D() {
+      return d3.map(this.data, this.defined);
+    },
+    //  if (defined === undefined)
+    //    defined = (d, i) => !isNaN(X[i]) && !isNaN(Y[i]);
+    //  const D = d3.map(data, defined);
+    //
+    //  // Compute default domains, and unique the z-domain.
+    //  if (xDomain === undefined) xDomain = d3.extent(X);
+    xDomain() {
+      return d3.extent(this.X);
+    },
+    //  if (yDomain === undefined)
+    //    yDomain = [0, d3.max(Y, (d) => (typeof d === "string" ? +d : d))];
+    yDomain() {
+      return [0, d3.max(this.Y, (d) => (typeof d === "string" ? +d : d))];
+    },
+    //  if (zDomain === undefined) zDomain = Z;
+    //  zDomain = new d3.InternSet(zDomain);
+    zDomain() {
+      return new d3.InternSet(this.Z);
+    },
+    //
+    //  // Omit any data not present in the z-domain.
+    //  const I = d3.range(X.length).filter((i) => zDomain.has(Z[i]));
+    I() {
+      return d3.range(this.X.length).filter((i) => this.zDomain.has(this.Z[i]));
+    },
+    //
+    //  // Construct scales and axes.
+    //  const xScale = xType(xDomain, xRange);
+    xScale() {
+      return this.chart.xType(this.xDomain, this.xRange);
+    },
+    //  const yScale = yType(yDomain, yRange);
+    yScale() {
+      return this.chart.yType(this.yDomain, this.yRange);
+    },
+    //  const xAxis = d3
+    //    .axisBottom(xScale)
+    //    .ticks(width / 80)
+    //    .tickSizeOuter(0);
+    xAxis() {
+      return d3
+        .axisBottom(this.xScale)
+        .ticks(this.chart.width / 80)
+        .tickSizeOuter(0);
+    },
+    //  const yAxis = d3.axisLeft(yScale).ticks(height / 60, yFormat);
+    yAxis() {
+      return d3
+        .axisLeft(this.yScale)
+        .ticks(this.chart.height / 60, this.chart.yFormat);
+    },
+    //
+    //  // Compute titles.
+    //  const T =
+    //    title === undefined ? Z : title === null ? null : d3.map(data, title);
+    T() {
+      return this.chart.title === undefined
+        ? this.Z
+        : this.chart.title === null
+        ? null
+        : d3.map(this.data, this.chart.title);
+    },
+    //  // Construct a line generator.
+    //  const line = d3
+    //    .line()
+    //    .defined((i) => D[i])
+    //    .curve(curve)
+    //    .x((i) => xScale(X[i]))
+    //    .y((i) => yScale(Y[i]));
+    line() {
+      return d3
+        .line()
+        .defined((i) => this.D[i])
+        .curve(this.chart.curve)
+        .x((i) => this.xScale(this.X[i]))
+        .y((i) => this.yScale(this.Y[i]));
+    },
   },
   methods: {
-    getScales() {
-      // All the maths to work chart co ordinates and woring out Axis
-      var parseDate = d3.timeParse("%m-%d-%Y");
-
-      this.data.forEach(function(d) {
-        d.date = parseDate(d.day);
-      });
-
-      const x = d3
-          .scaleTime()
-          .domain(
-              d3.extent(this.data, function(d) {
-                return d.date;
-              })
-          )
-          .rangeRound([0, this.chartDefaults.width - 100]);
-      const y = d3
-          .scaleLinear()
-          .domain([
-            0,
-            d3.max(this.data, function(d) {
-              return d.count + 100;
-            })
-          ])
-          .range([this.chartDefaults.height, 0]);
-      d3.axisBottom().scale(x);
-      d3.axisLeft().scale(y);
-
-      //Key funtions to draw X-axis,YAxis and the grid. All uses component axis
-      //play around with time format to get it to display as you want : d3.timeFormat("%b-%d")
-      var xAxis = d3
-          .axisBottom()
-          .scale(x)
-          .tickFormat(d3.timeFormat("%b"))
-          .tickValues(
-              this.data
-                  .map(function(d, i) {
-                    if (i > 0) {
-                      return d.date;
-                    }
-                    return false;
-                  })
-                  .splice(1)
-          )
-          .ticks(4);
-
-      var yAxis = d3
-          .axisLeft()
-          .scale(y)
-          .ticks(5);
-      var yGrid = d3
-          .axisLeft()
-          .scale(y)
-          .tickSize(-(this.chartDefaults.width - 100), 0, 0)
-          .tickFormat("");
-      // Return the key calculations and functions to draw the chart
-      return {
-        x,
-        y,
-        xAxis,
-        yAxis,
-        yGrid
-      };
+    renderChart() {
+      d3.select("#xaxis").call(this.xAxis);
+      d3.select("#yaxis")
+        .call(this.yAxis)
+        .call((g) => g.select(".domain").remove())
+        .call((g) =>
+          g
+            .selectAll(".tick line")
+            .clone()
+            .attr(
+              "x2",
+              this.chart.width - this.chart.marginLeft - this.chart.marginRight
+            )
+            .attr("stroke-opacity", 0.1)
+        )
+        .call((g) =>
+          g
+            .append("text")
+            .attr("x", -this.chart.marginLeft)
+            .attr("y", 10)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .text(this.chart.yLabel)
+        );
+      d3.select("#paths")
+        .selectAll("path")
+        .data(d3.group(this.I, (i) => this.Z[i]))
+        .join("path")
+        .style("mix-blend-mode", this.chart.mixBlendMode)
+        .attr(
+          "stroke",
+          typeof this.chart.color === "function"
+            ? ([z]) => this.chart.color(z)
+            : null
+        )
+        .attr("d", ([, I]) => this.line(I));
     },
-    getTrnsx(chartDefaults) {
-      //works out translate value in realtive to dynamic height
-      const t = "translate(0," + this.chartDefaults.height + ")";
-      return t;
+    pointermoved(event) {
+      const [xm, ym] = d3.pointer(event);
+      const i = d3.least(this.I, (i) =>
+        Math.hypot(this.xScale(this.X[i]) - xm, this.yScale(this.Y[i]) - ym)
+      ); // closest point
+      d3.select("#paths")
+        .selectAll("path")
+        .style("stroke", ([z]) => (this.Z[i] === z ? null : "#ddd"))
+        .filter(([z]) => this.Z[i] === z)
+        .raise();
+      d3.select("#dot")
+        .attr(
+          "transform",
+          `translate(${this.xScale(this.X[i])},${this.yScale(this.Y[i])})`
+        )
+        .attr("display", "block");
+      d3.select("#dot").select("text").text(this.T[i]);
+      d3.select("#svg")
+        .property("value", this.O[i])
+        .dispatch("input", { bubbles: true });
     },
-    calculatePath() {
-      //Get key calculation funtions to draw chart , Ie scale, axis mapping and plotting
-      const scale = this.getScales();
-      // Define calcultion to draw chart
-      const path = d3
-          .line()
-          .x(d => {
-            return scale.x(d.date);
-          })
-          .y(d => {
-            return scale.y(d.count);
-          })
-          .curve(d3.curveCardinal);
-
-      // draw line then this.line is injected into the template
-      this.line = path(this.data);
-    }
-  }
+    pointerentered() {
+      d3.select("#paths")
+        .selectAll("path")
+        .style("mix-blend-mode", null)
+        .style("stroke", "#ddd");
+      d3.select("#dot").attr("display", null);
+    },
+    pointerleft() {
+      d3.select("#paths")
+        .selectAll("path")
+        .style("mix-blend-mode", this.chart.mixBlendMode)
+        .style("stroke", null);
+      d3.select("#dot").attr("display", "none");
+      d3.select("#svg").value = null;
+      d3.select("#svg").dispatch("input", { bubbles: true });
+    },
+  },
 };
 </script>
-<!-- css loaderhttps://vue-loader.vuejs.org/guide/scoped-css.html#mixing-local-and-global-styles -->
+
 <style>
-text {
-  color: #fff;
+path {
+  color: #000;
+  mix-blend-mode: multiply;
 }
-
-path.line {
-  fill: none;
-  stroke: #ecbc3a;
-  stroke-width: 3px;
-}
-
-.grid line {
-  opacity: 0.05;
-}
-.xA line {
-  opacity: 0.5;
-}
-
-
-.ani2 svg .lineChart > path {
-  stroke: #ecbc3a;
-  -webkit-animation-name: draw-2;
-  animation-name: draw-2;
-}
-.ani1 svg .lineChart > path {
-  stroke: #ecbc3a;
-  -webkit-animation-name: draw;
-  animation-name: draw;
-}
-#Layer_1 {
-  width: 100%;
-}
-@-webkit-keyframes draw {
-  85% {
-  }
-  100% {
-    stroke-dashoffset: 0;
-  }
-}
-@keyframes draw {
-  85% {
-  }
-  100% {
-    stroke-dashoffset: 0;
-  }
-}
-
-@-webkit-keyframes draw-2 {
-  85% {
-  }
-  100% {
-    stroke-dashoffset: 0;
-  }
-}
-
-@keyframes draw-2 {
-  85% {
-  }
-  100% {
-    stroke-dashoffset: 0;
-  }
-}
-.text {
-  display: inline-block;
-  font-size: 3vw;
-  margin: 0.5vw 0 1.5vw;
-}
-
 svg {
-  background-color: transparent;
+  width: inherit;
+  height: inherit;
+  max-width: 100%;
+  max-height: 100%;
+  -webkit-tap-highlight-color: transparent;
+  color: #000;
 }
 </style>

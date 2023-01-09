d66b5855e031f28a4af31846a5b36af0d3c12ac4
Isabel Wanderwitz <isabel.wanderwitz@campus.lmu.de>
2023-01-09 21:35:11 +0100

    added toggle for hospitality and incidence

--- infoviz/src/components/Map/Map_block.vue
+++ infoviz/src/components/Map/Map_block.vue
@@ -1,15 +1,54 @@
 <template>
   <div>
-    map
+    <div id="header">
+      <h3 v-if="display_hospitality">Map: Hospitality</h3>
+      <h3 v-else>Map: Incidence</h3>
+      <div id="radio_buttons">
+        <input type="radio" @change="changeChart"
+               value="hospitality" name="display_hospitality" checked> Hospitality
+        <input type="radio" @change="changeChart"
+               value="incidence" name="display_hospitality"> Incidence
+      </div>
+    </div>
+    <!-- <LineChart v-if="lineChart" v-bind:class="[toggleClass]" />
+    <BarChart v-else v-bind:class="[toggleClass]" /> -->
   </div>
 </template>
 
 <script>
 
+import Map from "./Map.vue";
+
+
 export default {
-  name: "Map_block"
+  name: "Map_block",
+  components: {
+    Map,
+  },
+  data() {
+    return {
+      display_hospitality: true,
+      toggleClass: "ani1",
+    };
+  },
+  methods: {
+    play() {
+      // toggle classes to animate the line draw
+      this.toggleClass == "ani1"
+        ? (this.toggleClass = "ani2")
+        : (this.toggleClass = "ani1");
+    },
+    changeChart(event) {
+      // toggle hospitality
+      this.display_hospitality = !this.display_hospitality;
+    },
+  },
 }
+
+
+
 </script>
+
 
 <style scoped>
 

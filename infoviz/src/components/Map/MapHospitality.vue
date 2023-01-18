3354ce07518bdcc8fcf283c08a167520444e83a6
Robin Löbbert <robin.loebbert@hotmail.com>
2023-01-18 15:39:19 +0100

    movable line and correct timeslider

--- infoviz/src/components/Map/MapHospitality.vue
+++ infoviz/src/components/Map/MapHospitality.vue
@@ -1,14 +1,11 @@
 <template>
-  <svg 
+  <svg
     id="map_container"
     :width="map.width"
     :height="map.height"
     :viewBox="[0, 0, map.width, map.height]"
     v-bind:dataHospitality="dataHospitality"
-  >   
-
-
-  </svg>
+  ></svg>
 </template>
 
 <script>
@@ -21,144 +18,132 @@ import { useMeasuresStore } from "@/stores/politicalMeasures";
 import { regions } from "@/data/dataKeys";
 import { useDateStore } from "@/stores/selectedDate";
 
-
 // loading map data based on https://observablehq.com/@ch-bu/map-of-germany-unemployment-rate
-const mapDataGermany = topojson.feature(germany, germany.objects.states)
+const mapDataGermany = topojson.feature(germany, germany.objects.states);
 const mesh = topojson.mesh(germany, germany.objects.states, (a, b) => a !== b);
 
 // project and scale map
-var projection1 = d3.geoConicConformal()
-  .fitSize([650, 325], mesh);
+var projection1 = d3.geoConicConformal().fitSize([650, 325], mesh);
 
 var lastClickedRegion = "";
 
 export default {
   name: "vue-map",
-  components: { },  
+  components: {},
   data() {
-
-    const hospitalityStore1 = useHospitalityStore();
+    const hospitalityStore = useHospitalityStore();
     const politicalMeasures = useMeasuresStore();
-    var currentMonth = "2020-01"; // Start date
- 
+    const dateStore = useDateStore();
 
     return {
-
-      hospitalityStore1: hospitalityStore1,
-      politicalMeasures: politicalMeasures,
-      currentMonth: currentMonth,
+      hospitalityStore,
+      politicalMeasures,
+      dateStore,
       regions: regions,
       lastClickedRegion: lastClickedRegion,
       map: {
         width: 600, // outer width, in pixels
         height: 300, // outer height, in pixels
       },
-    }
-  }, 
+    };
+  },
   async mounted() {
-
-    await this.hospitalityStore1.initValues();
+    await this.hospitalityStore.initValues();
     await this.politicalMeasures.initValues();
 
     this.plotMapData();
     this.renderMap();
-
   },
   computed: {
     dataHospitality() {
-
-      const year = new Date(useDateStore().count * 1).getFullYear();
-      var month = new Date(useDateStore().count * 1).getMonth() + 1;
-
-      if (month < 10) {
-        month = "0" + month;
-      } else {
-        month = "" + month;
-      }
-
-      this.currentMonth = "" + year + "-" + month;
-
-      return this.hospitalityStore1.getRegionsByMonth(this.currentMonth).real.original;
+      return this.hospitalityStore.getRegionsByMonth(
+        this.dateStore.currentMonth
+      ).real.original;
     },
   },
   watch: {
-    dataHospitality: function() {
-
-      d3.select("#map_container")
-        .selectAll("g")
-        .remove();
+    dataHospitality: function () {
+      d3.select("#map_container").selectAll("g").remove();
 
       this.plotMapData();
-    }
+    },
   },
   methods: {
     renderMap() {
       d3.select("#map_container")
         .append("path")
         .datum(mapDataGermany)
-          .attr("fill", "none")
-          .attr("stroke", "#101010")
-          .attr("stroke-linejoin", "round")   
-          .attr("d", d3.geoPath().projection(projection1))
-          .attr("transform", "translate(-50, 0)")
-          .attr("id", "test");
+        .attr("fill", "none")
+        .attr("stroke", "#101010")
+        .attr("stroke-linejoin", "round")
+        .attr("d", d3.geoPath().projection(projection1))
+        .attr("transform", "translate(-50, 0)")
+        .attr("id", "test");
     },
     plotMapData(lastClickedRegion) {
       // chose filling
       // var myColor = d3.scaleQuantize([0, 100], d3.schemeOranges[6]);
-    
+
       // linear filling incremented in steps of 10
-      var myColor =  d3.scaleLinear().domain([0, 100]) 
+      var myColor = d3
+        .scaleLinear()
+        .domain([0, 100])
         .range(["white", "orange"], 10);
 
-       // <!-- hatch for lockdowns based on https://jsfiddle.net/sqrz3/    -->
-  // <defs>
-  //   <pattern id="stripe" patternUnits="userSpaceOnUse" width="40" height="20">
-  //     <line x1="10" y1="0" x2="30" y2="20" />
-  //     <line x1="-10" y1="0" x2="10" y2="20" />
-  //     <line x1="30" y1="0" x2="50" y2="20" />
-  //   </pattern>
-  //   <mask id="mask">
-  //     <rect height="500" width="500" style="fill: url(#stripe)" />
-  //   </mask>
-  // </defs>
+      // <!-- hatch for lockdowns based on https://jsfiddle.net/sqrz3/    -->
+      // <defs>
+      //   <pattern id="stripe" patternUnits="userSpaceOnUse" width="40" height="20">
+      //     <line x1="10" y1="0" x2="30" y2="20" />
+      //     <line x1="-10" y1="0" x2="10" y2="20" />
+      //     <line x1="30" y1="0" x2="50" y2="20" />
+      //   </pattern>
+      //   <mask id="mask">
+      //     <rect height="500" width="500" style="fill: url(#stripe)" />
+      //   </mask>
+      // </defs>
 
       d3.select("#map_container")
-      .append("g")
-      // .attr("id", "ha");
-      // d3.select("#ha")
-      // .append("defs");
-      // d3.select("#ha")
-      // .selectAll("defs")
-      // .append("pattern")
-      // .attr({"id": "stripe", patternUnits: "userSpaceOnUse", width:"40", height:"20"})
-      // .append("rect")
-      // .attr({x1:"10", y1:"0", x2:"30", y2:"20"})
-      // .append("rect")
-      // .attr({x1:"-10", y1:"0", x2:"10", y2:"20"})
-      // .append("rect")
-      // .attr({x1:"30", y1:"0", x2:"50", y2:"20"})
-      .selectAll("path")
-      .data(mapDataGermany.features)
-      .join("path")
-      .attr("fill", d => isNaN(this.dataHospitality[d.properties.nameEN]) ? "#686464" : myColor(this.dataHospitality[d.properties.nameEN]))
-      // .attr("fill", "url(#stripe)")
-      .attr("fill-opacity", 1)
-      .attr("d", d3.geoPath().projection(projection1))
-      .attr("transform", "translate(-50, 0)")
-      .attr("id", d => d.properties.nameEN)
-      //
-  
-      // visually display clicked region
-      .on("click", function(lastClickedRegion){
-            return function(){ 
+        .append("g")
+        // .attr("id", "ha");
+        // d3.select("#ha")
+        // .append("defs");
+        // d3.select("#ha")
+        // .selectAll("defs")
+        // .append("pattern")
+        // .attr({"id": "stripe", patternUnits: "userSpaceOnUse", width:"40", height:"20"})
+        // .append("rect")
+        // .attr({x1:"10", y1:"0", x2:"30", y2:"20"})
+        // .append("rect")
+        // .attr({x1:"-10", y1:"0", x2:"10", y2:"20"})
+        // .append("rect")
+        // .attr({x1:"30", y1:"0", x2:"50", y2:"20"})
+        .selectAll("path")
+        .data(mapDataGermany.features)
+        .join("path")
+        .attr("fill", (d) =>
+          isNaN(this.dataHospitality[d.properties.nameEN])
+            ? "#686464"
+            : myColor(this.dataHospitality[d.properties.nameEN])
+        )
+        // .attr("fill", "url(#stripe)")
+        .attr("fill-opacity", 1)
+        .attr("d", d3.geoPath().projection(projection1))
+        .attr("transform", "translate(-50, 0)")
+        .attr("id", (d) => d.properties.nameEN)
+        //
+
+        // visually display clicked region
+        .on(
+          "click",
+          (function (lastClickedRegion) {
+            return function () {
               // reset
               if (lastClickedRegion != "" || lastClickedRegion == this.id) {
                 d3.select("#" + lastClickedRegion)
                   .attr("stroke", "#101010")
-                  .attr("stroke-width", "0.5")
-              };
-             
+                  .attr("stroke-width", "0.5");
+              }
+
               if (lastClickedRegion != this.id) {
                 d3.select(this)
                   .attr("stroke-width", "3")
@@ -168,11 +153,12 @@ export default {
               } else {
                 lastClickedRegion = "";
               }
-            }   
-        }(this.lastClickedRegion));
+            };
+          })(this.lastClickedRegion)
+        );
     },
   },
-}
+};
 </script>
 
 <style>

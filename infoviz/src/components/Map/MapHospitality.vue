de79022b1f2e49eb6c275f089c7cb6d6922e2c71
Isabel Wanderwitz <isabel.wanderwitz@campus.lmu.de>
2023-01-18 12:44:18 +0100

    added legend

--- infoviz/src/components/Map/MapHospitality.vue
+++ infoviz/src/components/Map/MapHospitality.vue
@@ -1,11 +1,15 @@
 <template>
-  <svg
+  <svg 
     id="map_container"
     :width="map.width"
     :height="map.height"
     :viewBox="[0, 0, map.width, map.height]"
     v-bind:dataHospitality="dataHospitality"
-  ></svg>
+  >   
+  </svg>
+  <div>
+  <svg id="legend_hospitality" height=300 width=450 color="green"></svg>
+</div>
 </template>
 
 <script>
@@ -18,132 +22,206 @@ import { useMeasuresStore } from "@/stores/politicalMeasures";
 import { regions } from "@/data/dataKeys";
 import { useDateStore } from "@/stores/selectedDate";
 
+
 // loading map data based on https://observablehq.com/@ch-bu/map-of-germany-unemployment-rate
-const mapDataGermany = topojson.feature(germany, germany.objects.states);
+const mapDataGermany = topojson.feature(germany, germany.objects.states)
 const mesh = topojson.mesh(germany, germany.objects.states, (a, b) => a !== b);
 
 // project and scale map
-var projection1 = d3.geoConicConformal().fitSize([650, 325], mesh);
+var projection1 = d3.geoConicConformal()
+  .fitSize([650, 325], mesh);
 
 var lastClickedRegion = "";
 
 export default {
   name: "vue-map",
-  components: {},
+  components: { },  
   data() {
-    const hospitalityStore = useHospitalityStore();
+
+    const hospitalityStore1 = useHospitalityStore();
     const politicalMeasures = useMeasuresStore();
-    const dateStore = useDateStore();
+    var currentMonth = "2020-01"; // Start date
+ 
 
     return {
-      hospitalityStore,
-      politicalMeasures,
-      dateStore,
+
+      hospitalityStore1: hospitalityStore1,
+      politicalMeasures: politicalMeasures,
+      currentMonth: currentMonth,
       regions: regions,
       lastClickedRegion: lastClickedRegion,
       map: {
         width: 600, // outer width, in pixels
         height: 300, // outer height, in pixels
       },
-    };
-  },
+    }
+  }, 
   async mounted() {
-    await this.hospitalityStore.initValues();
+
+    await this.hospitalityStore1.initValues();
     await this.politicalMeasures.initValues();
 
     this.plotMapData();
     this.renderMap();
+
   },
   computed: {
     dataHospitality() {
-      return this.hospitalityStore.getRegionsByMonth(
-        this.dateStore.currentMonth
-      ).real.original;
+
+      const year = new Date(useDateStore().count * 1).getFullYear();
+      var month = new Date(useDateStore().count * 1).getMonth() + 1;
+
+      if (month < 10) {
+        month = "0" + month;
+      } else {
+        month = "" + month;
+      }
+
+      this.currentMonth = "" + year + "-" + month;
+
+      return this.hospitalityStore1.getRegionsByMonth(this.currentMonth).real.original;
     },
   },
   watch: {
-    dataHospitality: function () {
-      d3.select("#map_container").selectAll("g").remove();
+    dataHospitality: function() {
+
+      d3.select("#map_container")
+        .selectAll("g")
+        .remove();
 
       this.plotMapData();
-    },
+    }
   },
   methods: {
     renderMap() {
       d3.select("#map_container")
         .append("path")
         .datum(mapDataGermany)
-        .attr("fill", "none")
-        .attr("stroke", "#101010")
-        .attr("stroke-linejoin", "round")
-        .attr("d", d3.geoPath().projection(projection1))
-        .attr("transform", "translate(-50, 0)")
-        .attr("id", "test");
+          .attr("fill", "none")
+          .attr("stroke", "#101010")
+          .attr("stroke-linejoin", "round")   
+          .attr("d", d3.geoPath().projection(projection1))
+          .attr("transform", "translate(-50, 0)")
+          .attr("id", "test")
+    
+    // create legend for map
+    var legendColor = d3.select("#map_container")  
+    var missingValueColor = d3.select("#map_container")
+    var myColor =  d3.scaleLinear().domain([0, 100]) 
+            .range(["white", "orange"], 11);
+            var keys = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    var rectSize = 20
+
+   // rects to display color values in legend
+  legendColor.selectAll("legendRect")
+    .data(keys)
+    .enter()
+    .append("rect")
+      .attr("x", 50)
+      .attr("y", function(d, i){ return 100 + i*(rectSize)})
+      .attr("width", rectSize)
+      .attr("height", rectSize)
+      .style("fill", function(d){ return myColor(d)})
+      .attr("transform", "translate(400, -100)")
+
+    // text for each color
+    legendColor.selectAll("legendLabels")
+    .data(keys)
+    .enter()
+    .append("text")
+    .attr("x", 50 + rectSize*1.2)
+    .attr("y", function(d,i){ return 100 + i*(rectSize) + (rectSize/2)})
+    .style("fill", "black")
+    .text(function(d){ return d})
+    .attr("text-anchor", "left")
+    .style("alignment-baseline", "middle")
+    .attr("transform", "translate(400, -100)")
+
+    // customized rect for not available data
+    missingValueColor.selectAll("legendValueMissing")
+    legendColor.selectAll("legendRect")
+    .data(keys)
+    .enter()
+    .append("rect")
+      .attr("x", 50)
+      .attr("y", 100)
+      .attr("width", rectSize)
+      .attr("height", rectSize)
+      .style("fill", "#686464")
+      .attr("transform", "translate(400, -130)")
+
+    // customized text for not available data
+    missingValueColor.selectAll("legendValueMissing")
+    legendColor.selectAll("legendLabels")
+    .data(keys)
+    .enter()
+    .append("text")
+    .attr("x", 50 + rectSize*1.2)
+    .attr("y", 110) 
+    .style("fill", "black")
+    .text("Data not available")
+    .attr("text-anchor", "left")
+    .style("alignment-baseline", "middle")
+    .attr("transform", "translate(400, -130)");
     },
+
     plotMapData(lastClickedRegion) {
       // chose filling
       // var myColor = d3.scaleQuantize([0, 100], d3.schemeOranges[6]);
-
+    
       // linear filling incremented in steps of 10
-      var myColor = d3
-        .scaleLinear()
-        .domain([0, 100])
+      var myColor =  d3.scaleLinear().domain([0, 100]) 
         .range(["white", "orange"], 10);
 
-      // <!-- hatch for lockdowns based on https://jsfiddle.net/sqrz3/    -->
-      // <defs>
-      //   <pattern id="stripe" patternUnits="userSpaceOnUse" width="40" height="20">
-      //     <line x1="10" y1="0" x2="30" y2="20" />
-      //     <line x1="-10" y1="0" x2="10" y2="20" />
-      //     <line x1="30" y1="0" x2="50" y2="20" />
-      //   </pattern>
-      //   <mask id="mask">
-      //     <rect height="500" width="500" style="fill: url(#stripe)" />
-      //   </mask>
-      // </defs>
+       // <!-- hatch for lockdowns based on https://jsfiddle.net/sqrz3/    -->
+        // <defs>
+        //   <pattern id="stripe" patternUnits="userSpaceOnUse" width="40" height="20">
+        //     <line x1="10" y1="0" x2="30" y2="20" />
+        //     <line x1="-10" y1="0" x2="10" y2="20" />
+        //     <line x1="30" y1="0" x2="50" y2="20" />
+        //   </pattern>
+        //   <mask id="mask">
+        //     <rect height="500" width="500" style="fill: url(#stripe)" />
+        //   </mask>
+        // </defs>
 
       d3.select("#map_container")
-        .append("g")
-        // .attr("id", "ha");
-        // d3.select("#ha")
-        // .append("defs");
-        // d3.select("#ha")
-        // .selectAll("defs")
-        // .append("pattern")
-        // .attr({"id": "stripe", patternUnits: "userSpaceOnUse", width:"40", height:"20"})
-        // .append("rect")
-        // .attr({x1:"10", y1:"0", x2:"30", y2:"20"})
-        // .append("rect")
-        // .attr({x1:"-10", y1:"0", x2:"10", y2:"20"})
-        // .append("rect")
-        // .attr({x1:"30", y1:"0", x2:"50", y2:"20"})
-        .selectAll("path")
-        .data(mapDataGermany.features)
-        .join("path")
-        .attr("fill", (d) =>
-          isNaN(this.dataHospitality[d.properties.nameEN])
-            ? "#686464"
-            : myColor(this.dataHospitality[d.properties.nameEN])
-        )
-        // .attr("fill", "url(#stripe)")
-        .attr("fill-opacity", 1)
-        .attr("d", d3.geoPath().projection(projection1))
-        .attr("transform", "translate(-50, 0)")
-        .attr("id", (d) => d.properties.nameEN)
-        //
-
-        // visually display clicked region
-        .on(
-          "click",
-          (function (lastClickedRegion) {
-            return function () {
+      .append("g")
+      // try to add hatch
+      // .attr("id", "ha");
+      // d3.select("#ha")
+      // .append("defs");
+      // d3.select("#ha")
+      // .selectAll("defs")
+      // .append("pattern")
+      // .attr({"id": "stripe", patternUnits: "userSpaceOnUse", width:"40", height:"20"})
+      // .append("rect")
+      // .attr({x1:"10", y1:"0", x2:"30", y2:"20"})
+      // .append("rect")
+      // .attr({x1:"-10", y1:"0", x2:"10", y2:"20"})
+      // .append("rect")
+      // .attr({x1:"30", y1:"0", x2:"50", y2:"20"})
+      .selectAll("path")
+      .data(mapDataGermany.features)
+      .join("path")
+      .attr("fill", d => isNaN(this.dataHospitality[d.properties.nameEN]) ? "#686464" : myColor(this.dataHospitality[d.properties.nameEN]))
+      // .attr("fill", "url(#stripe)")
+      .attr("fill-opacity", 1)
+      .attr("d", d3.geoPath().projection(projection1))
+      .attr("transform", "translate(-50, 0)")
+      .attr("id", d => d.properties.nameEN)
+      //
+  
+      // visually display clicked region
+      .on("click", function(lastClickedRegion){
+            return function(){ 
               // reset
               if (lastClickedRegion != "" || lastClickedRegion == this.id) {
                 d3.select("#" + lastClickedRegion)
                   .attr("stroke", "#101010")
-                  .attr("stroke-width", "0.5");
-              }
-
+                  .attr("stroke-width", "0.5")
+              };
+             
               if (lastClickedRegion != this.id) {
                 d3.select(this)
                   .attr("stroke-width", "3")
@@ -153,12 +231,11 @@ export default {
               } else {
                 lastClickedRegion = "";
               }
-            };
-          })(this.lastClickedRegion)
-        );
+            }   
+        }(this.lastClickedRegion));
     },
   },
-};
+}
 </script>
 
 <style>
--- infoviz/src/components/Map/MapIncidence.vue
+++ infoviz/src/components/Map/MapIncidence.vue
@@ -1,10 +1,11 @@
 <template>
-  <svg
+  <svg 
     id="map_container"
     :width="map.width"
     :height="map.height"
     :viewBox="[0, 0, map.width, map.height]"
-  ></svg>
+  >      
+  </svg>
 </template>
 
 <script>
@@ -17,30 +18,33 @@ import { germanyKey, regions } from "@/data/dataKeys";
 import { useDateStore } from "@/stores/selectedDate";
 
 // loading map data based on https://observablehq.com/@ch-bu/map-of-germany-unemployment-rate
-const mapDataGermany = topojson.feature(germany, germany.objects.states);
+const mapDataGermany = topojson.feature(germany, germany.objects.states)
 const mesh = topojson.mesh(germany, germany.objects.states, (a, b) => a !== b);
 
 // project and scale map
-var projection1 = d3.geoConicConformal().fitSize([650, 325], mesh);
+var projection1 = d3.geoConicConformal()
+.fitSize([650, 325], mesh);
 
 export default {
   name: "vue-map",
-  components: {},
+  components: { },  
   data() {
-    const covidCasesStore = useCovidCasesStore(); // TODO: this date should be adjusted according to the current slider position.
-    const dateStore = useDateStore();
+
+    const covidCasesStore1 = useCovidCasesStore();
+    var currentDay = "2020-01-07"; // TODO: this date should be adjusted according to the current slider position.
+    
     return {
-      covidCasesStore,
+      covidCasesStore1: covidCasesStore1,
+      currentDay: currentDay,
       regions: regions,
-      dateStore,
       map: {
         width: 600, // outer width, in pixels
         height: 300, // outer height, in pixels
       },
-    };
-  },
+    }
+  }, 
   async mounted() {
-    await this.covidCasesStore.initValues();
+    await this.covidCasesStore1.initValues();
 
     this.plotMapData();
     this.renderMap();
@@ -51,62 +55,139 @@ export default {
     retrieveMaxIncidence() {
       var maxIncidences = [];
 
-      for (var state in this.covidCasesStore.cases) {
+      for (var state in this.covidCasesStore1.cases) {
         if (state != germanyKey) {
+
           var stateIncidence = [];
 
           stateIncidence.push(
-            ...this.covidCasesStore.cases[state].map((value) => {
+            ...this.covidCasesStore1.cases[state].map((value) => {
               return value.value;
-            })
-          );
-
+            }));
+        
           maxIncidences.push(Math.max(...stateIncidence));
+
         }
-      }
+      };
 
       return Math.max(...maxIncidences);
+
     },
     dataIncidence() {
+
+      // retrieve current time stamp
+      const year = new Date(useDateStore().count * 1).getFullYear();
+      var month = new Date(useDateStore().count * 1).getMonth() + 1;
+      var day = new Date(useDateStore().count * 1).toLocaleDateString("en-US", {day: "numeric"});
+
+      if (month < 10) {
+        month = "0" + month;
+      } else {
+        month = "" + month;
+      }
+
+      if (day < 10) {
+        day = "0" + day;
+      } else {
+        day = "" + day;
+      }
+
+      this.currentDay = "" + year + "-" + month + "-" + day;
+
       // load current incidence value per state per day (currentDay)
       var data = {};
 
-      for (var state in this.covidCasesStore.cases) {
-        if (state != germanyKey) {
-          const v = this.covidCasesStore.cases[state]
-            .filter((element) => {
-              if (isNaN(element.day)) {
-                return element.day == this.dateStore.currentDate;
-              } else {
-                return element.day == this.dateStore.currentTimestamp;
-              }
-            })
-            .map((value) => {
+        for (var state in this.covidCasesStore1.cases) {
+          if (state != germanyKey) {
+            const v = this.covidCasesStore1.cases[state].filter(element => element.day == new Date(this.currentDay).getTime()).map((value) => {
               return value.value;
             });
-          data[state] = v;
+            data[state] = v;
+          }
         }
-      }
-      return data;
+        return data;
     },
   },
   watch: {
-    dataIncidence: function () {
-      d3.select("#map_container").selectAll("g").remove();
+    dataIncidence: function() {
+
+      d3.select("#map_container")
+        .selectAll("g")
+        .remove();
 
       this.plotMapData();
-    },
+    }
   },
   methods: {
     renderMap() {
       d3.select("#map_container")
         .append("path")
         .datum(mapDataGermany)
-        .attr("fill", "none")
-        .attr("stroke", "#101010")
-        .attr("stroke-linejoin", "round")
-        .attr("d", d3.geoPath().projection(projection1))
-        .attr("transform", "translate(-50, 0)");
+          .attr("fill", "none")
+          .attr("stroke", "#101010")
+          .attr("stroke-linejoin", "round")   
+          .attr("d", d3.geoPath().projection(projection1))
+          .attr("transform", "translate(-50, 0)")
+
+     // create legend for map
+    var legendColor = d3.select("#map_container")  
+    var missingValueColor = d3.select("#map_container")
+    var myColor =  d3.scaleLinear().domain([0, this.retrieveMaxIncidence]) 
+            .range(["white", "blue"], 2);
+    var keys = ["0",Math.round(this.retrieveMaxIncidence*0.25), Math.round(this.retrieveMaxIncidence*0.5), Math.round(this.retrieveMaxIncidence*0.75), Math.round(this.retrieveMaxIncidence)]
+    var rectSize = 20
+
+   // rects to display color values in legend
+   legendColor.selectAll("legendRect")
+    .data(keys)
+    .enter()
+    .append("rect")
+      .attr("x", 50)
+      .attr("y", function(d, i){ return 100 + i*(rectSize)})
+      .attr("width", rectSize)
+      .attr("height", rectSize)
+      .style("fill", function(d){ return myColor(d)})
+      .attr("transform", "translate(400, -100)")
+
+    // text for each color
+    legendColor.selectAll("legendLabels")
+    .data(keys)
+    .enter()
+    .append("text")
+    .attr("x", 50 + rectSize*1.2)
+    .attr("y", function(d,i){ return 100 + i*(rectSize) + (rectSize/2)})
+    .style("fill", "black")
+    .text(function(d){ return d})
+    .attr("text-anchor", "left")
+    .style("alignment-baseline", "middle")
+    .attr("transform", "translate(400, -100)")
+
+    // customized rect for not available data
+    missingValueColor.selectAll("legendValueMissing")
+    legendColor.selectAll("legendRect")
+    .data(keys)
+    .enter()
+    .append("rect")
+      .attr("x", 50)
+      .attr("y", 100)
+      .attr("width", rectSize)
+      .attr("height", rectSize)
+      .style("fill", "#686464")
+      .attr("transform", "translate(400, -130)")
+
+      // customized text for not available data
+      missingValueColor.selectAll("legendValueMissing")
+      legendColor.selectAll("legendLabels")
+    .data(keys)
+    .enter()
+    .append("text")
+    .attr("x", 50 + rectSize*1.2)
+    .attr("y", 110) 
+    .style("fill", "black")
+    .text("Data not available")
+    .attr("text-anchor", "left")
+    .style("alignment-baseline", "middle")
+    .attr("transform", "translate(400, -130)")
     },
 
     plotMapData() {
@@ -118,36 +199,32 @@ export default {
       //   .range(["white", "blue"]);
 
       // linear filling incremented in steps of 250
-      var myColor = d3
-        .scaleLinear()
-        .domain([0, this.retrieveMaxIncidence])
+      var myColor =  d3.scaleLinear().domain([0, this.retrieveMaxIncidence]) 
         .range(["white", "blue"], 250);
-
+    
       d3.select("#map_container")
-        .append("g")
-        .selectAll("path")
-        .data(mapDataGermany.features)
-        .join("path")
-        .attr("fill", (d) => myColor(this.dataIncidence[d.properties.nameEN]))
+      .append("g")
+      .selectAll("path")
+      .data(mapDataGermany.features)
+      .join("path")
+      .attr("fill", d => myColor(this.dataIncidence[d.properties.nameEN]))
 
-        // filling for logarithmic scale
-        //.attr("fill", d => myColor(Math.log(this.dataIncidence[d.properties.nameEN])))
-        .attr("fill-opacity", 1)
-        .attr("d", d3.geoPath().projection(projection1))
-        .attr("transform", "translate(-50, 0)")
-        .attr("id", (d) => d.properties.nameEN)
-        // visually display clicked region
-        .on(
-          "click",
-          (function (lastClickedRegion) {
-            return function () {
+      // filling for logarithmic scale
+      //.attr("fill", d => myColor(Math.log(this.dataIncidence[d.properties.nameEN])))   
+      .attr("fill-opacity", 1)
+      .attr("d", d3.geoPath().projection(projection1))
+      .attr("transform", "translate(-50, 0)")
+      .attr("id", d => d.properties.nameEN)
+      // visually display clicked region
+      .on("click", function(lastClickedRegion){
+            return function(){ 
               // reset
               if (lastClickedRegion != "" || lastClickedRegion == this.id) {
                 d3.select("#" + lastClickedRegion)
                   .attr("stroke", "#101010")
-                  .attr("stroke-width", "0.5");
-              }
-
+                  .attr("stroke-width", "0.5")
+              };
+             
               if (lastClickedRegion != this.id) {
                 d3.select(this)
                   .attr("stroke-width", "3")
@@ -157,12 +234,11 @@ export default {
               } else {
                 lastClickedRegion = "";
               }
-            };
-          })(this.lastClickedRegion)
-        );
+            }   
+        }(this.lastClickedRegion));
     },
   },
-};
+}
 </script>
 
 <style>

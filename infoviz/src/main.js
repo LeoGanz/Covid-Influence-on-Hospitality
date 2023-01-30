ae585337b15bb0e3ec1b407627d408dfd6eac357
Leonard Ganz <leonard@ruediger-ganz.de>
2023-01-30 23:05:08 +0100

    Add Fontawesome icons

--- infoviz/src/main.js
+++ infoviz/src/main.js
@@ -5,6 +5,9 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faSackDollar, faVirusCovid} from "@fortawesome/free-solid-svg-icons";
 
 const app = createApp(App)
 
@@ -12,5 +15,8 @@ const pinia = createPinia()
 pinia.use(({ store }) => { store.$router = markRaw(router) })
 app.use(pinia)
 app.use(router)
+
+library.add(faVirusCovid, faSackDollar)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')

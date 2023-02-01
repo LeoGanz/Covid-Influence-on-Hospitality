60012ad95c3dee4a2aa3738498983887884328b9
Robin Löbbert <robin.loebbert@hotmail.com>
2023-02-01 18:42:07 +0100

    corrected color for lockdown

--- infoviz/src/components/Map/MapIncidence.vue
+++ infoviz/src/components/Map/MapIncidence.vue
@@ -30,7 +30,7 @@ export default {
   name: "vue-map",
   components: {},
   data() {
-    const covidCasesStore = useCovidCasesStore(); 
+    const covidCasesStore = useCovidCasesStore();
     const dateStore = useDateStore();
     const currentRegion = useCurrentRegionStore();
     const politicalMeasures = useMeasuresStore();
@@ -54,11 +54,10 @@ export default {
     this.renderMap();
 
     if (this.currentRegion.currentRegionName != "Germany") {
-     
-                d3.select("#" + this.currentRegion.currentRegionName )
-                  .attr("stroke-width", "2")
-                  .attr("stroke", "black");
-              }
+      d3.select("#" + this.currentRegion.currentRegionName)
+        .attr("stroke-width", "2")
+        .attr("stroke", "black");
+    }
   },
   computed: {
     // function to detect max value of incidence over time, as a reference value
@@ -85,40 +84,42 @@ export default {
     dataIncidence() {
       // load current incidence value per state per day (currentDay)
       var data = {};
-      console.log(this.covidCasesStore.cases)
+      console.log(this.covidCasesStore.cases);
       for (var state in this.covidCasesStore.cases) {
         if (state != germanyKey) {
           // console.log(this.dateStore.currentDate)
 
           const v = this.covidCasesStore.cases[state]
-            .filter((element) => element.day == this.dateStore.currentDate || element.day == new Date(this.dateStore.currentDate).getTime())
+            .filter(
+              (element) =>
+                element.day == this.dateStore.currentDate ||
+                element.day == new Date(this.dateStore.currentDate).getTime()
+            )
             .map((value) => {
               return value.value;
             });
           data[state] = v;
         }
       }
-      console.log(data)
+      console.log(data);
       return data;
     },
   },
   watch: {
     dataIncidence: function () {
-
       if (firstLoad) {
-        firstLoad = false
+        firstLoad = false;
       } else {
-
-        console.log("jetzt")
+        console.log("jetzt");
         d3.select("#incidence_container").selectAll("g").remove();
 
         this.plotMapData();
 
         if (this.currentRegion.currentRegionName != "Germany") {
-          d3.select("#" + this.currentRegion.currentRegionName )
+          d3.select("#" + this.currentRegion.currentRegionName)
             .attr("stroke-width", "2")
             .attr("stroke", "black");
-          }
+        }
       }
     },
   },
@@ -140,7 +141,18 @@ export default {
         .scaleLinear()
         .domain([0, this.retrieveMaxIncidence])
         .range(["white", "orange"], 1);
-      var colorKeys = ["Data not available", "Ongoing Lockdown*", "", "0", "250", "750", "1250", "1750", "2250", "2750"];
+      var colorKeys = [
+        "Data not available",
+        "Ongoing Lockdown*",
+        "",
+        "0",
+        "250",
+        "750",
+        "1250",
+        "1750",
+        "2250",
+        "2750",
+      ];
       var keys = ["0", "250", "750", "1250", "1750", "2250", "2750"];
       var rectSize = 20;
 
@@ -179,8 +191,8 @@ export default {
         .style("alignment-baseline", "middle")
         .attr("transform", "translate(375, -60)");
 
-     // customized rect for not available data
-     missingValueColor.selectAll("legendValueMissing");
+      // customized rect for not available data
+      missingValueColor.selectAll("legendValueMissing");
       legendColor
         .selectAll("legendRect")
         .data(keys)
@@ -194,8 +206,8 @@ export default {
         .style("stroke", "#999999")
         .attr("transform", "translate(371, 92)");
 
-       // customized rect for not available data
-    missingValueColor.selectAll("legendLockdown");
+      // customized rect for not available data
+      missingValueColor.selectAll("legendLockdown");
       legendColor
         .selectAll("legendRect")
         .data(keys)
@@ -210,13 +222,15 @@ export default {
     },
 
     isLockdown(state) {
-      const lockdownData = this.politicalMeasures.lockdown[state].filter((el) => el.day == this.dateStore.currentDate);
+      const lockdownData = this.politicalMeasures.lockdown[state].filter(
+        (el) => el.day == this.dateStore.currentDate
+      );
       if (lockdownData.length == 0) {
-        return false
+        return false;
       } else if (lockdownData[0].value > 0) {
-        return true
+        return true;
       } else {
-        return false
+        return false;
       }
     },
 
@@ -234,29 +248,33 @@ export default {
         .domain([0, this.retrieveMaxIncidence])
         .range(["white", "orange"], 250);
 
-        d3.select("#incidence_container")
-          .append("defs")
-          .append("pattern")
-          .attr("id", "diagonalHatch")
-          .attr("patternUnits", "userSpaceOnUse")
-          .attr("width", 8)
-          .attr("height", 8)
-          .append("path")
-          .attr("d", "M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2")
-          .style("stroke", "black")
-          .style("stroke-width", 1);
+      d3.select("#incidence_container")
+        .append("defs")
+        .append("pattern")
+        .attr("id", "diagonalHatch")
+        .attr("patternUnits", "userSpaceOnUse")
+        .attr("width", 8)
+        .attr("height", 8)
+        .append("path")
+        .attr("d", "M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2")
+        .style("stroke", "#999999")
+        .style("stroke-width", 1);
 
-        d3.select("#incidence_container")
+      d3.select("#incidence_container")
         .append("g")
         .selectAll("path")
         .data(mapDataGermany.features)
         .join("path")
-        .style("fill", (d) => this.isLockdown(d.properties.nameEN) ? "url(#diagonalHatch)" : "transparent")
+        .style("fill", (d) =>
+          this.isLockdown(d.properties.nameEN)
+            ? "url(#diagonalHatch)"
+            : "transparent"
+        )
         .attr("fill-opacity", 1)
         .attr("d", d3.geoPath().projection(projection1))
         .attr("transform", "translate(-80, 0)");
 
-        d3.select("#incidence_container")
+      d3.select("#incidence_container")
         .append("g")
         .selectAll("path")
         .data(mapDataGermany.features)
@@ -274,11 +292,13 @@ export default {
           "click",
           (function (regionStore) {
             return function () {
-
               const lastClickedRegion = regionStore.currentRegionName;
 
               // reset
-              if (lastClickedRegion != "Germany" || lastClickedRegion != this.id) {              
+              if (
+                lastClickedRegion != "Germany" ||
+                lastClickedRegion != this.id
+              ) {
                 d3.select("#" + lastClickedRegion)
                   .attr("stroke", "#999999")
                   .attr("stroke-width", "0.5");
@@ -289,10 +309,12 @@ export default {
                   .attr("stroke-width", "2")
                   .attr("stroke", "#222222");
 
-                const region_after = mapDataGermany.features.filter((feature) => feature.properties.name == this.id)
-                regionStore.updateRegion(region_after[0].properties.nameEN)
+                const region_after = mapDataGermany.features.filter(
+                  (feature) => feature.properties.name == this.id
+                );
+                regionStore.updateRegion(region_after[0].properties.nameEN);
               } else {
-                regionStore.updateRegion("germany")
+                regionStore.updateRegion("germany");
               }
             };
           })(this.currentRegion)
